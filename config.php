50e14650519ed9fd0837b797ba3b37a32ac84792
NeedleStacker <dickjon3s00@gmail.com>
2026-04-26 19:15:37 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,15 @@
 <?php
 // config.php
+define('SITE_NAME', 'NazivObrta');
+define('OWNER_NAME', 'ImeVlasnika');
+define('SITE_EMAIL', 'info@domena.hr');
+define('SITE_PHONE', '+385 91 234 5678');
+define('SITE_ADDRESS', 'Ulica 1, 99000 Nigdje');
+define('SITE_MAP_QUERY', 'Ulica+1,+Nigdje,+Croatia');
+define('DEFAULT_DESCRIPTION', 'Obrt - Vaš stručnjak za obrtnistvo.');
+define('DEFAULT_KEYWORDS', 'keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword');
 return [
-    'SMTP_PASSWORD' => 'tajna',
-    'SMTP_PORT' => 111
+    'SMTP_PASSWORD' => 'LozinkaMaila',
+    'SMTP_HOST' => 'mail.domena.hr',
+    'SMTP_PORT' => 465
 ];
