1026c014c90eaf7793c5ab2fa0377f267ddd7d58
NeedleStacker <dickjon3s00@gmail.com>
2026-04-26 23:05:41 +0200

    Add files via upload

--- includes/captcha_helper.php
+++ includes/captcha_helper.php
@@ -8,21 +8,16 @@ function getCaptchaQuestion() {
     $n2 = rand(1, 5);
     $sum = $n1 + $n2;
 
-    $words = [
-        0 => 'nula',
-        1 => 'jedan',
-        2 => 'dva',
-        3 => 'tri',
-        4 => 'četiri',
-        5 => 'pet',
-        6 => 'šest',
-        7 => 'sedam',
-        8 => 'osam',
-        9 => 'devet',
-        10 => 'deset'
+    $map = [
+        0=>'nula',1=>'jedan',2=>'dva',3=>'tri',4=>'četiri',
+        5=>'pet',6=>'šest',7=>'sedam',8=>'osam',9=>'devet',10=>'deset'
     ];
 
-    $_SESSION['captcha_answer'] = $words[$sum];
+    if (isset($map[$sum])) {
+        $_SESSION['captcha_answer'] = $map[$sum];
+    } else {
+        $_SESSION['captcha_answer'] = 'nepoznato';
+    }
+
     return "$n1 + $n2 = ?";
 }
-?>
